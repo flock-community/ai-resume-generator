241f19ec5a71f21692a80c5dcc7c97e1983b589b
Tim Smelik <timsmelik@gmail.com>
2023-10-19 11:11:40 +0200

    Fix package

--- src/main/kotlin/community/flock/ai/resume/generator/AiResumeGeneratorApplication.kt
+++ src/main/kotlin/community/flock/ai/resume/generator/AiResumeGeneratorApplication.kt
@@ -1,4 +1,4 @@
-package community.flock.resume
+package community.flock.ai.resume.generator
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
