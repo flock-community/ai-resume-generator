ed87ea71ff366b7f8cf2ed021e12975474195512
Florian Putker <florian.putker@flock.community>
2023-10-19 13:15:07 +0200

    merge renamed service and template

---
+++ src/main/kotlin/community/flock/ai/resume/generator/FormController.kt
@@ -0,0 +1,28 @@
+package community.flock.ai.resume.generator
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+
+@Controller
+class FormController(val promptService: PromptService) {
+
+    @GetMapping("/prompt")
+    fun getPrompt(model: Model): String {
+        model.addAttribute("prompt", Prompt(""))
+        return "prompt"
+    }
+
+    @PostMapping("/prompt")
+    fun postPrompt(@ModelAttribute prompt: Prompt, model: Model): String {
+        val result = Prompt(promptService.generateResumeExperienceItem().text)
+        model.addAttribute("prompt", result)
+        return "result"
+    }
+}
+
+data class Prompt(
+    val content: String
+)
