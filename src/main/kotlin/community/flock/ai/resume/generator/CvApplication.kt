e852ca1f235ca5dd620d2a0afee896419d1a8e4f
Florian Putker <florian.putker@flock.community>
2023-10-19 10:07:11 +0200

    initialise

---
+++ src/main/kotlin/community/flock/ai/resume/generator/CvApplication.kt
@@ -0,0 +1,11 @@
+package community.flock.resume
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiResumeGeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiResumeGeneratorApplication>(*args)
+}
---
+++ src/test/kotlin/community/flock/ai/resume/generator/CvApplicationTests.kt
@@ -0,0 +1,13 @@
+package community.flock.ai.resume.generator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CvApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
