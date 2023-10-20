f6de9905fc3230e2c2c0b548ce8dcea4c83452c2
Florian Putker <florian.putker@flock.community>
2023-10-20 15:42:46 +0200

    working h2

--- src/main/kotlin/community/flock/ai/resume/generator/FlockerController.kt
+++ src/main/kotlin/community/flock/ai/resume/generator/FlockerController.kt
@@ -2,16 +2,17 @@ package community.flock.ai.resume.generator
 
 import community.flock.wirespec.generated.Flocker
 import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class FlockerController(val flockerService: FlockerService){
 
     @PutMapping("/flocker")
-    fun store(flocker: Flocker): Flocker = flockerService.store(flocker)
+    fun store(@RequestBody flocker: Flocker): Flocker = flockerService.store(flocker)
 }
 
 @Service
--- src/main/kotlin/community/flock/ai/resume/generator/model.kt
+++ src/main/kotlin/community/flock/ai/resume/generator/model.kt
@@ -2,13 +2,25 @@ package community.flock.ai.resume.generator
 
 import community.flock.wirespec.generated.Flocker
 import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import java.util.UUID
 
 @Entity
-class DbFlocker (
-    val firstName: String,
-    val lastName: String,
-    val dateOfBirth: String,
-    val phoneNumber: Int
-)
+data class DbFlocker(
+    @Id
+    val id: String = UUID.randomUUID().toString(),
+    val firstName: String?,
+    val lastName: String?,
+    val dateOfBirth: String?,
+    val phoneNumber: Int?
+) {
+    constructor() : this(
+        firstName = null,
+        lastName = null,
+        dateOfBirth = null,
+        phoneNumber = null
+    )
+}
 
-fun Flocker.toDbModel() = DbFlocker(firstName, lastName, dateOfBirth, phoneNumber)
+fun Flocker.toDbModel() =
+    DbFlocker(firstName = firstName, lastName = lastName, dateOfBirth = dateOfBirth, phoneNumber = phoneNumber)
