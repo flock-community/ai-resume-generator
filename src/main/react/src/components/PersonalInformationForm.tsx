7fd243c36f6664333cb0ab8363c81ca97121b636
Florian Putker <florian.putker@flock.community>
2024-01-31 16:47:53 +0100

    gitss

--- src/main/react/src/components/PersonalInformationForm.tsx
+++ src/main/react/src/components/PersonalInformationForm.tsx
@@ -2,29 +2,35 @@ import {useForm} from "react-hook-form";
 import {Flocker} from "../api/types.ts";
 
 export const PersonalInformationForm = () => {
-    const {register, handleSubmit, formState: {errors}} = useForm();
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const onSubmit = async (flocker: Flocker) => {
+        console.log(flocker);
+        await saveFlocker(flocker)
+    };
 
     return (
         <form onSubmit={handleSubmit(data => saveFlocker(data))}>
             <div>
                 First Name <input defaultValue="Al" {...register("firstName", {required: true})} />
             </div>
+            {errors.firstName && <span>This field is required</span>}
             <div>
                 Last Name <input defaultValue="Coholic" {...register("lastName", {required: true})}/>
             </div>
+            {errors.lastName && <span>This field is required</span>}
             <div>
                 Date of Birth <input defaultValue="01-01-2000" {...register("dateOfBirth")} />
             </div>
             <div>
                 Phone number <input type="number" defaultValue="0612345678" {...register("phoneNumber")} />
             </div>
-            {errors.exampleRequired && <span>This field is required</span>}
             <input type={"submit"}/>
         </form>
     )
 }
 
 export function saveFlocker(flocker: Flocker) {
+
     const requestOptions: RequestInit = {
         method: 'PUT',
         headers: {
